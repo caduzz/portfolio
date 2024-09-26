0e6285cce91c2f62789f385307e53d5166d8aa62
caduzz <cadu0970@gmail.com>
2024-09-26 19:41:39 -0300

    Header component done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,72 @@
 import { Nav } from "./styles"
 
+import {
+  SiGithub,
+  SiGmail,
+  SiInstagram,
+  SiLinkedin
+} from "react-icons/si"
+
 function Header() {
   return (
     <Nav>
-
+      <div>
+        <div className="navBarArea">
+          <div className="navBarItems">
+            <a
+              onClick={() => scrollToSection(inicioSecaoRef)}
+              className="navBarItem"
+            >
+              Home
+            </a>
+            <a
+              onClick={() => scrollToSection(sobreSecaoRef)}
+              className="navBarItem"
+            >
+              About
+            </a>
+            <a
+              onClick={() => scrollToSection(progetoSecaoRef)}
+              className="navBarItem"
+            >
+              Projects
+            </a>
+            <a href="#" className="navBarItem">
+              Articles
+            </a>
+          </div>
+          <div className="navBarItems">
+            <a
+              className="navBarItem"
+              href="https://twitter.com/cadu__s"
+              target="_blank" rel="noreferrer"
+            >
+              <SiGmail size={22} />
+            </a>
+            <a
+              className="navBarItem"
+              href="https://www.linkedin.com/in/cadu-dev"
+              target="_blank" rel="noreferrer"
+            >
+              <SiLinkedin size={22} />
+            </a>
+            <a
+              className="navBarItem"
+              href="https://github.com/caduzz"
+              target="_blank" rel="noreferrer"
+            >
+              <SiGithub size={22} />
+            </a>
+            <a
+              className="navBarItem"
+              href="https://www.instagram.com/cadu.dev"
+              target="_blank" rel="noreferrer"
+            >
+              <SiInstagram size={22} />
+            </a>
+          </div>
+        </div>
+      </div>
     </Nav>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,37 @@
 import styled from "styled-components";
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 75px;
+
+  padding: 25px;
+
+  .navBarArea {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+
+    .navBarItems {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      flex-direction: row;
+      width: 50%;
+      height: 75px;
+
+      .navBarItem {
+        margin: 0 15px;
+        color: #fff;
+      }
+      .navBarItem:hover {
+        color: #348cf0;
+      }
+    }
+  }
+`;
